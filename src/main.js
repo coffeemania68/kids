8fec845382293142c6a75d32d765d4fea8a2f0e9
coffeemania68 <gmschool0245@gmail.com>
2025-01-19 16:08:34 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './components/Button.js'
 import './components/Card.js'
-import { initGame } from './utils/gameData.js'
+import { initGame, getGameData } from './utils/gameData.js'
 import { loadGameProgress } from './utils/localStorage.js';
 
 // 카카오 SDK 초기화
@@ -9,84 +9,32 @@ Kakao.init(kakaoApiKey);
 
 //  init Game Data
 initGame();
+ const gameData = getGameData();
 
 const root = document.getElementById('root');
 
 function renderMainScreen() {
-    const mainScreenHTML = `
-    <div class="max-w-4xl mx-auto">
-        <h1 class="text-3xl font-bold mb-6 text-center">12종 게임 시리즈</h1>
+  const mainScreenHTML = `
+   <div class="max-w-4xl mx-auto">
+          <h1 class="text-3xl font-bold mb-6 text-center">12종 게임 시리즈</h1>
 
             <div class="grid grid-cols-3 gap-4 mb-8">
-                <div class="p-4">
-                    <button data-gameid="gumiho-school">
-                        <img src="/public/images/thumbnails/gumiho-school_thumbnail.png" alt="구미호 학교에 간 날 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
+               ${gameData.games.map(game => `
+                 <div class="p-4">
+                     <button data-gameid="${game.id}">
+                         <img src="${game.thumbnail}" alt="${game.title} 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
                     </button>
                 </div>
-                <div class="p-4">
-                    <button  data-gameid="dokkebi-delivery">
-                        <img src="/public/images/thumbnails/dokkebi-delivery_thumbnail.png" alt="도깨비와 배달 아르바이트 썸네일"  class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-            <div class="p-4">
-                    <button data-gameid="dragon-brother">
-                        <img src="/public/images/thumbnails/dragon-brother_thumbnail.png" alt="용으로 변신한 우리 오빠 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button  data-gameid="ai-cinderella">
-                        <img src="/public/images/thumbnails/ai-cinderella_thumbnail.png" alt="AI 로봇과 신데렐라 썸네일"  class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="metaverse-snowwhite">
-                        <img src="/public/images/thumbnails/metaverse-snowwhite_thumbnail.png" alt="메타버스 속 백설공주 썸네일"  class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button  data-gameid="youtuber-mermaid">
-                        <img src="/public/images/thumbnails/youtuber-mermaid_thumbnail.png" alt="유튜버가 된 인어공주 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="plastic-witch">
-                        <img src="/public/images/thumbnails/plastic-witch_thumbnail.png" alt="플라스틱 섬의 마녀 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="dust-alice">
-                        <img src="/public/images/thumbnails/dust-alice_thumbnail.png" alt="미세먼지 나라의 앨리스 썸네일"  class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="global-warming-red">
-                        <img src="/public/images/thumbnails/global-warming-red_thumbnail.png" alt="지구온난화와 빨간모자 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="moonlight-hanbok">
-                        <img src="/public/images/thumbnails/moonlight-hanbok_thumbnail.png" alt="달빛 한복과 금도끼 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="sunmoon-game">
-                        <img src="/public/images/thumbnails/sunmoon-game_thumbnail.png" alt="해님달님 게임대회 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
-                <div class="p-4">
-                    <button data-gameid="vr-simcheong">
-                        <img src="/public/images/thumbnails/vr-simcheong_thumbnail.png" alt="심청이의 VR 여행 썸네일" class="rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300">
-                    </button>
-                </div>
+              `).join('')}
+            </div>
+            <footer class="text-center mt-6 text-gray-500 text-sm">
+               <p>지금까지 <span id="playerCount">0</span>명이 참여했어요!</p>
+               <p class="mb-2">주의사항: 저장된 데이터는 브라우저를 닫거나 삭제하면 초기화됩니다.</p>
+               <p> 제작/광고문의: <a href="mailto:petsmoa2000@gmail.com">petsmoa2000@gmail.com</a></p>
+                   <p class="mt-1"> © Test Lab. All Rights Reserved.</p>
+            </footer>
         </div>
-        <footer class="text-center mt-6 text-gray-500 text-sm">
-          <p>지금까지 <span id="playerCount">0</span>명이 참여했어요!</p>
-            <p class="mb-2">주의사항: 저장된 데이터는 브라우저를 닫거나 삭제하면 초기화됩니다.</p>
-         <p> 제작/광고문의: <a href="mailto:petsmoa2000@gmail.com">petsmoa2000@gmail.com</a></p>
-             <p class="mt-1"> © Test Lab. All Rights Reserved.</p>
-        </footer>
-    </div>
-    `;
+        `;
     root.innerHTML = mainScreenHTML;
 
     // Game Select Event
@@ -107,18 +55,48 @@ function renderMainScreen() {
 
 // 게임 페이지 로드 함수
 function loadGameScreen(gameId){
-console.log(gameId);
+   console.log(gameId);
+   const selectedGame = gameData.games.find(game => game.id === gameId);
 
     const gameScreenHTML = `
         <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-pink-100">
-        <div class="bg-white p-8 rounded-lg shadow-xl max-w-md mx-auto">
-            <h2 class="text-2xl font-bold mb-4">${gameId}</h2>
+            <div class="bg-white p-8 rounded-lg shadow-xl max-w-md mx-auto">
+            <h2 class="text-2xl font-bold mb-4">${selectedGame.title}</h2>
             <p class="mb-4">게임 설명 들어가는 곳</p>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">게임 시작</button>
-        </div>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-sceneid="${selectedGame.scenes[0].id}" onClick="loadScene(this,'${gameId}')">게임 시작</button>
+            </div>
         </div>
     `;
-
     root.innerHTML = gameScreenHTML;
+
+}
+
+function loadScene(el,gameId){
+    const sceneId = el.dataset.sceneid;
+    const selectedGame = gameData.games.find(game => game.id === gameId);
+    const selectedScene = selectedGame.scenes.find(scene => scene.id === sceneId);
+
+    const sceneHTML = `
+        <div class="min-h-screen bg-gradient-to-br from-blue-100 to-pink-100 p-6 flex items-center justify-center">
+          <div class="max-w-2xl mx-auto bg-white/80 backdrop-blur p-6 rounded-lg">
+              <h2 class="text-2xl font-bold text-center mb-6">${selectedGame.title}</h2>
+              <p class="text-lg mb-6">${selectedScene.text}</p>
+             <div class="space-y-4">
+              ${selectedScene.options.map(option => `
+                <button
+                     onClick="loadScene(this,'${gameId}')"
+                   data-sceneid="${option.nextSceneId}"
+                   class="w-full py-4 text-lg bg-gradient-to-r from-green-500 to-yellow-500 hover:from-green-600 hover:to-yellow-600 group"
+                  >
+                    <span class="group-hover:scale-110 transition-transform inline-block">
+                    ${option.text}
+                    </span>
+                </button>
+               `).join('')}
+            </div>
+          </div>
+        </div>
+    `;
+    root.innerHTML = sceneHTML;
 }
 renderMainScreen();
