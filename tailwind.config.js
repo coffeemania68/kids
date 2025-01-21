84f53ec5d7882b79b08ae04e231ec6d8c1853b44
coffeemania68 <gmschool0245@gmail.com>
2025-01-21 13:24:28 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
       "./index.html",
      "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  theme: {
-    extend: {},
+theme: {
+    extend: {
+      colors: {
+        'royal-gold': '#D4AF37',
+        'soft-gold': '#F2D584',
+        'soft-navy': '#334B77',
+        'light-bg': '#F0F6FF',
+      },
+    },
   },
   plugins: [],
 }
